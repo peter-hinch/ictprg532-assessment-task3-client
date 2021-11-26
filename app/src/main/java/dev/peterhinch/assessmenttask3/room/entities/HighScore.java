ee3b1c882075ed18c7c7cd768f75c1ff09813d9b
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-26 12:09:36 +1100

    Add getters and setters, override toString() method.

--- app/src/main/java/dev/peterhinch/assessmenttask3/room/entities/HighScore.java
+++ app/src/main/java/dev/peterhinch/assessmenttask3/room/entities/HighScore.java
@@ -1,5 +1,6 @@
 package dev.peterhinch.assessmenttask3.room.entities;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Ignore;
@@ -25,5 +26,42 @@ public class HighScore {
   public HighScore (int score, Date date) {
     this.score = score;
     this.date = date;
+  }
+
+  @Ignore
+  public HighScore (int id, int score, Date date) {
+    this.id = id;
+    this.score = score;
+    this.date = date;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  @NonNull
+  @Override
+  public String toString() {
+    return "id: " + this.id + "\nscore: " + this.score + "\ndate: " + this.date;
   }
 }
